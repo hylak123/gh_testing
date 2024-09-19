185f9ce6d44eecde21a351f9cdcb9ba43709f294
Maciej <maciej.chylinski@gmail.com>
2024-09-19 12:14:18 +0200

    Add testdata for Linux

--- test_github.py
+++ test_github.py
@@ -70,9 +70,40 @@ if "Windows" in platform.system():
         },
     }
 else:
+    HOME = r"/home/runner/work/gh_testing"
     TEST_DATA = {
-        "test_repo_init": {"repo_path_local": r"/home/runner/work/gh_testing/gh_testing"},
-        }
+        "test_repo_init": {"repo_path_local": HOME},
+        "test_open_existing_repo": {"repo_path_local": HOME},
+        "test_clone_remote_repo_https": {
+            "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+        },
+        "test_commit": {
+            "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+            "commit_msg": content_generator("commit_msg", 10),
+        },
+        "test_create_new_branch": {
+            "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+            "new_branch_name": content_generator("text", 20),
+        },
+        "test_switch_branch": {
+            # "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+            "existing_branch_name": content_generator("text", 20),
+        },
+        "test_pull": {
+            "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+            "existing_branch_name": "main",
+        },
+        "test_push": {
+            # "remote_repo_url": REPO_URL_HTTPS,
+            "repo_path_local": HOME,
+            "existing_branch_name": "main",
+        },
+    }
 
 
 class TestGithub:
